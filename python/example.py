124c600ee33dbfe2f30cb67d4db3d9a436206279
Sam Bryant <sam.bryant@armakuni.com>
2020-11-18 14:58:29 +0000

    Remove example.py

--- python/example.py
+++
@@ -1,9 +0,0 @@
-import logging
-import sys
-
-handler = logging.StreamHandler(stream=sys.stdout)
-logger = logging.getLogger()
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-logger.info("My info log message")
-logger.warning("foobar")
